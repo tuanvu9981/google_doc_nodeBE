d04e65a67e780880455df5824c751ab14b91ff6b
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2023-03-18 22:44:43 +0700

    fix: repair document api by adding '/'

--- routes/document.js
+++ routes/document.js
@@ -3,7 +3,7 @@ const documentRouter = express.Router();
 const Document = require('../models/document');
 const auth = require('../middleware/auth');
 
-documentRouter.post('doc/create', auth, async (req, res) => {
+documentRouter.post('/doc/create', auth, async (req, res) => {
     try {
         const { createdAt } = req.body;
         let document = new Document({
@@ -18,10 +18,33 @@ documentRouter.post('doc/create', auth, async (req, res) => {
     }
 });
 
-documentRouter.get('doc/me', auth, async (req, res) => {
+// all documents of 1 user
+documentRouter.get('/doc/me', auth, async (req, res) => {
     try {
-        let documents = Document.find({ uid: req.user });
+        let documents = await Document.find({ uid: req.user });
+        console.log(documents);
         res.json({ documents });
+    } catch (e) {
+        res.json({ message: e.message });
+    }
+});
+
+// update 1 document
+documentRouter.put('/doc/update-title', auth, async (req, res) => {
+    try {
+        const { id, title } = req.body;
+        const document = await Document.findByIdAndUpdate(id, { title });
+        res.json({ document });
+    } catch (e) {
+        res.json({ message: e.message });
+    }
+});
+
+// get detail of 1 document
+documentRouter.get('/doc/:id', auth, async (req, res) => {
+    try {
+        let document = await Document.findById(req.params.id);
+        res.json({ document });
     } catch (e) {
         res.json({ message: e.message });
     }
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const auth = require("../middleware/auth");
 const authRouter = express.Router();
 require('dotenv').config();
 const JWT_KEY = process.env.JWT_KEY;
+const JWT_EXPIRATION = process.env.JWT_EXPIRATION;
 
 authRouter.post('/api/signup', async (req, res) => {
     try {
@@ -20,7 +21,7 @@ authRouter.post('/api/signup', async (req, res) => {
             });
             user = await user.save();
         }
-        const token = jwt.sign({ id: user._id }, JWT_KEY);
+        const token = jwt.sign({ id: user._id }, JWT_KEY, { expiresIn: JWT_EXPIRATION });
         res.json({ user, token });
         // equal to res.json({user})
     } catch (e) {
